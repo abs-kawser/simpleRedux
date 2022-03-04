f9e6a164447441234792b8ba6ad772368c024a77
abs kawser <abskawser77000@gmail.com>
2022-03-04 23:47:12 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import './App.css'
+import { useSelector, useDispatch } from 'react-redux';
+import { incNumber, decNumber } from './action/index';
 
-function App() {
+
+const App = () => {
+  const myState = useSelector((state) => state.changeTheNumber)
+  const dispatch = useDispatch();
+  console.log(myState);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+
+        <h1>Increment/Decrement counter</h1>
+        <h4>using React and Redux</h4>
+
+        <div className="quantity">
+          <a className="quantity__minus" title="Decrement" onClick={() => dispatch(decNumber())}><span>-</span></a>
+          <input name="quantity" type="number" className="quantity__input" value={myState} />
+          <a className="quantity__plus" title="Increment" onClick={() => dispatch(incNumber())}><span>+</span></a>
+        </div>
+
+      </div>
+
+    </>
   );
-}
+};
+
 
 export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+//import kora holo 
+import changeTheNumber from './Updown';
+
+
+import { combineReducers } from 'redux';
+
+ const rootReducer=combineReducers({
+    changeTheNumber,
+
+ })
+ console.log(combineReducers);
+ export default rootReducer ;
